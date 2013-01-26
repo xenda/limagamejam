1cc8289275e33d11966b89d6c0e63bf07dcb1242
otakurzo <renzo.vista@gmail.com>
2013-01-26 11:33:17 -0500

    Bat: Chapter two - The Fly

--- game_objects/bat.rb
+++ game_objects/bat.rb
@@ -2,19 +2,22 @@ class Bat < GameObject
   trait :bounding_box, :debug => false #, :scale => 0.7
   trait :collision_detection
 
+  attr_accessor :hunting
+
   def setup
 
+  	@hunting = false
     @state = :sleeping
     @direction = :right
 
-    @animations = Animation.new(:file => "media/bat.png", :size => [50,46], :delay => 200);
+    @animations = Animation.new(:file => "media/bat.png", :size => [50,46], :delay => 120);
     @animations.frame_names = {
     	:sleeping => 0, 
     	:fly_left => 1..4, 
     	:fly_right => 5..8 
     }
 
-    @image = @animations[:sleeping]
+    @image = @animations[@state]
 
     self.rotation_center = :top_center
 
@@ -27,11 +30,13 @@ class Bat < GameObject
   end
 
   def update
+  	case @state
+  		when :sleeping
+			@image = @animations[@state]
+		when :fly_right, :fly_left
+			@image = @animations[@state].next
 
-  end
-
-  def draw
-
+	end
   end
 
   def move(x, y)
@@ -39,7 +44,10 @@ class Bat < GameObject
     self.y = y
   end
 
+  
   def bat_fly
-  	puts :batfly
+  	@state = :fly_right
+
+  	puts @hunting
   end
 end
