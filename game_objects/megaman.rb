0e2d9996d3a47f2639cd3ce0a4eaa2df8ac0237e
otakurzo <renzo.vista@gmail.com>
2013-01-26 08:06:37 -0500

    Megaman HERO! NOTE: Replace Doctor with Megaman in level1.rb

---
+++ game_objects/megaman.rb
@@ -0,0 +1,125 @@
+class Megaman < Chingu::GameObject
+  trait :bounding_box, :scale => 0.8
+  traits :timer, :collision_detection , :timer, :velocity
+
+  def setup
+    self.input = {
+      :holding_left => :move_to_left,
+      :holding_right => :move_to_right,
+      :holding_up => :jump
+    }
+
+    @state = :normal
+    @direction = :right
+
+
+    @animations = {}
+    @animations[:normal] = Animation.new(:file => "media/megaman-sprites-normal.png", :size => [72,72], :delay => 200);
+    @animations[:normal].frame_names = { :left => 0..1, :right => 2..3 }
+
+    @animations[:run] = Animation.new(:file => "media/megaman-sprites-run.png", :size => [72,72], :delay => 80);
+    @animations[:run].frame_names = { :left => 0..2, :right => 3..5 }
+
+    @animations[:jump] = Animation.new(:file => "media/megaman-sprites-jump.png", :size => [90,90]);
+    @animations[:jump].frame_names = { :left => 0..1, :right => 2..3 }
+
+    @image =  current_animation.first
+
+    @jumping = false
+    @died = false
+
+    self.zorder = 300
+    self.acceleration_y = 0.5
+    self.max_velocity = 10
+    self.rotation_center = :bottom_center
+
+    update
+    cache_bounding_box
+  end
+
+  def current_animation
+    @animations[@state][@direction]
+  end
+
+  def move_to_left
+    if @jumping
+      @state = :jump
+    else
+      @state = :run  
+    end
+
+    @direction = :left
+
+    if self.x >= 30
+      move(-5, 0)
+    end
+    #@image = Image["doctor-left.png"]
+  end
+
+  def move_to_right
+    if @jumping
+      @state = :jump
+    else
+      @state = :run  
+    end
+    
+    @direction = :right
+
+    if self.x < 3200
+      move(5, 0)
+    end
+    #@image = Image["doctor-right.png"]
+  end
+
+  def jump
+    return if @jumping
+
+    @state = :jump
+    @jumping = true
+    self.velocity_y = -10
+  end
+
+  def move(x, y)
+    self.x += x
+    self.y += y
+  end
+
+  def die
+    @died = true
+    self.velocity_y = -10
+  end
+
+  def parapadeo
+    orientation = (@direction==:right) ? 'first' : 'last';
+    
+    if @image == @animations[:normal][@direction].send( "#{orientation}".to_sym )
+      return if Random.rand(10) != 0
+    end
+
+    @image = @animations[:normal][@direction].next
+  end
+
+  def update
+
+    puts @state
+
+    if !(self.holding_any? :left, :right, :jump) && !@jumping
+      @state = :normal
+      parapadeo
+    else
+      @image = @animations[@state][@direction].next
+    end
+
+    self.each_collision(Floor) do |me, block|
+      if self.velocity_y < 0
+        me.y = block.bb.bottom + me.image.height * self.factor_y
+        self.velocity_y = 0
+      else
+        unless @died
+          @jumping = false
+          me.y = block.bb.top
+        end
+      end
+    end
+  end
+end
